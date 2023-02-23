509d546f946c3729b393a020eb29dca365975caa
Matt Bonney <bonneym@gmail.com>
2023-02-22 20:33:09 -0500

    More examples of what can be done. Showing summaries or date ranges in headers as you scroll.

--- SwiftUIListOnAppear/SwiftUIListOnAppear/ContentView.swift
+++ SwiftUIListOnAppear/SwiftUIListOnAppear/ContentView.swift
@@ -10,10 +10,14 @@ import SwiftUI
 public struct Item: Identifiable, Hashable {
     public let id: Int
     public let title: String
+    public let date: Date
 
     public init(id: Int, title: String) {
         self.id = id
         self.title = title
+
+        // Generate the timestamp.
+        self.date = Calendar.current.date(byAdding: .day, value: -1 * id, to: Date.now) ?? .now
     }
 }
 
@@ -28,7 +32,7 @@ struct ItemView: View {
     let item: Item
 
     var body: some View {
-        LabeledContent(item.title, value: item.id, format: .number)
+        LabeledContent(item.title, value: item.date, format: .dateTime.month().day())
     }
 }
 
@@ -36,6 +40,34 @@ struct ContentView: View {
     var items: [Item] = Item.previewCollection()
 
     @State private var visibleItems = Set<Item>()
+
+    var visibleItemRange: ClosedRange<Int> {
+        let items = Array(visibleItems)
+                        .sorted { $0.id < $1.id }
+                        .compactMap { $0.id }
+
+        let low = items.first ?? 0
+        let high = items.last ?? 0
+
+        return low...high
+    }
+
+    var visibleItemsAtStart: Bool {
+        visibleItemRange.contains(0)
+    }
+
+    var visibleItemDateRangeText: Text {
+        let items = Array(visibleItems)
+            .sorted { $0.id < $1.id }
+            .compactMap { $0.date }
+
+        let first = items.first ?? Date.now
+        let last = items.last ?? Date.now
+
+        let t1 = Text(first, format: .dateTime.month().day())
+        let t2 = Text(last, format: .dateTime.month().day())
+        return t1 + Text(" to ") + t2
+    }
 
     var visibleItemDescription: String {
         Array(visibleItems)
@@ -64,23 +96,21 @@ struct ContentView: View {
     }
 
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                Text("Visible: ")
-                    .bold()
-                Text(visibleItemRangeDescription)
-                    .monospaced()
-            }
-            .padding()
-
+        NavigationStack {
             List {
-                ForEach(items) { item in
-                    ItemView(item: item)
-                        .animation(.easeOut, value: visibleItems)
-                        .onAppear { visibleItems.insert(item) }
-                        .onDisappear { visibleItems.remove(item) }
+                Section {
+                    ForEach(items) { item in
+                        ItemView(item: item)
+                            .animation(.easeOut, value: visibleItems)
+                            .onAppear { visibleItems.insert(item) }
+                            .onDisappear { visibleItems.remove(item) }
+                    }
+                } header: {
+                    Text(visibleItemRangeDescription)
                 }
             }
+            .listStyle(.plain)
+            .navigationTitle(visibleItemDateRangeText)
         }
     }
 }
